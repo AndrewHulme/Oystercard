4cffe340852936077312cf051ded298ca1194e98
dpwdec <declankolakowski@gmail.com>
2020-04-21 15:26:21 +0100

    Refactored oystercard spec file to use method describe blocks and shared contexts

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
 require './lib/oystercard'
 
+shared_context "above minimum" do
+  let(:subject) do
+    subject = described_class.new
+    subject.top_up(50)
+    subject
+  end
+end
+
 describe Oystercard do
   limit = Oystercard::LIMIT
   min_fare = Oystercard::MIN_FARE
@@ -8,7 +16,11 @@ describe Oystercard do
     expect(subject).to respond_to(:balance)
   end
 
-  describe "top_up method" do
+  it "checks if a card can show if it's in_journey?" do
+    expect(subject).to respond_to(:in_journey)
+  end
+
+  describe "#top_up" do
     it "tops up the card with a specified balance" do
       subject.top_up(10)
       expect(subject.balance).to eq 10
@@ -20,35 +32,38 @@ describe Oystercard do
     end
   end
 
-  it "deducts specified amount of money from the card" do
-    subject.top_up(50)
-    subject.deduct(5)
-    expect(subject.balance).to eq 45
+  describe "#deduct" do
+    it "deducts specified amount of money from the card" do
+      subject.top_up(50)
+      subject.deduct(5)
+      expect(subject.balance).to eq 45
+    end
   end
 
-  it "checks that it can be touched in" do
-    expect(subject).to respond_to(:touch_in)
+  describe '#touch_in' do
+    context "balance is" do
+      include_context 'above minimum'
+      it "checks if in_journey is true if card is touched in" do
+        subject.touch_in
+        expect(subject.in_journey).to eq true
+      end
+    end
+
+    context "card has less than £#{min_fare}" do
+      it "raises an error" do
+        expect { subject.touch_in }.to raise_error("Insufficient balance to travel, at least £#{min_fare} needed.")
+      end
+    end
   end
 
-  it "checks if a card can show if it's in_journey?" do
-    expect(subject).to respond_to(:in_journey)
+  describe '#touch_out' do
+    context "balance is" do
+      include_context 'above minimum'
+      it "checks if in_journey is false if card is touched out" do
+        subject.touch_in
+        subject.touch_out
+        expect(subject.in_journey).to be false
+      end
+    end
   end
-
-  it "checks if in_journey is true if card is touched in" do
-    subject.top_up(50)
-    subject.touch_in
-    expect(subject.in_journey).to eq true
-  end
-
-  it "checks if in_journey is false if card is touched out" do
-    subject.top_up(50)
-    subject.touch_in
-    subject.touch_out
-    expect(subject.in_journey).to be_falsey
-  end
-
-  it "expects touch_in to raise an error if the card has less than £#{min_fare} balance" do
-    expect{ subject.touch_in }.to raise_error("Insufficient balance to travel, at least £#{min_fare} needed.")
-  end
-
 end
